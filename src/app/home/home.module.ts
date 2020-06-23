c47de2125ce9e34814a51f9fcd73ed4946b6cda8
NataliaVolik <log.litana@gmail.com>
2020-06-23 21:44:16 +0600

    #5 в компонент выводится список акций и добавлен autocomplete

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ShareListComponent } from './share-list/share-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
 
 const routes: Routes = [
   {
@@ -13,10 +20,16 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     HomeComponent,
+    ShareListComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
   ],
   exports: [
     HomeComponent,
