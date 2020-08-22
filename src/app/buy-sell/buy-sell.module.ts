bcb08665cd7624af3c68f27bcb055e444343147c
gordondalos <esbondarenko@ticketbox.ru>
2020-08-22 09:14:02 +0600

    У каждого пользователя свой токен

---
+++ src/app/buy-sell/buy-sell.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BuySellComponent } from './buy-sell/buy-sell.component';
+
+
+
+@NgModule({
+  declarations: [BuySellComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BuySellModule { }
