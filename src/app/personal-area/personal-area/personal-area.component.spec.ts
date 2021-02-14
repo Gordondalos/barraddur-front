b45d6ce2642a1457581e86102f7551b5137f35d0
gordondalos <esbondarenko@ticketbox.ru>
2021-02-14 22:45:16 +0600

    update

--- src/app/personal-area/personal-area/personal-area.component.spec.ts
+++ src/app/personal-area/personal-area/personal-area.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PersonalAreaComponent } from './personal-area.component';
 
@@ -6,7 +6,7 @@ describe('PersonalAreaComponent', () => {
   let component: PersonalAreaComponent;
   let fixture: ComponentFixture<PersonalAreaComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PersonalAreaComponent ]
     })
