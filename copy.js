4c2fe6521e0092cf40b69140b3b47b9853393dcb
gordondalos <esbondarenko@ticketbox.ru>
2021-02-16 22:44:17 +0600

    Перезагрузка сервиса

--- copy.js
+++ copy.js
@@ -22,7 +22,13 @@ const copyRecursiveSync = function (src, dest) {
 
 const src = './dist/tinkoff-front'
 const extDir = '../www/html'
+
 // fs.rmdirSync(extDir, { recursive: true });
-rimraf(extDir, function () { console.log("done"); });
+rimraf(extDir, function () { console.log("Dir html removed"); });
+
+fs.mkdir(extDir, function (){
+  console.log('Dir html created')
+});
+
 const dest = '../www/html';
 copyRecursiveSync(src, dest)
