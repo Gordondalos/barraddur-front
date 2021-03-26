b28a9675e84929145dada3a78a02ee0c5bdc1ff1
gordondalos <esbondarenko@ticketbox.ru>
2021-03-27 02:49:54 +0600

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   hmr: false,
   baseHref: '/',
-  API_REST_URL: 'http://baraddur.ru',
+  API_REST_URL: 'https://baraddur.ru',
   API_REST_PORT: '3000',
-  SOCKET_URL: 'http://baraddur.ru:5000'
+  SOCKET_URL: 'https://baraddur.ru:5000'
 };
