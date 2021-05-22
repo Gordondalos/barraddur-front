e64224e2a925c383565cd247f65a1f82e7fe3e09
gordondalos <esbondarenko@ticketbox.ru>
2021-05-22 18:48:55 +0600

    Отображение информации регистрации

--- src/app/how-get-tocken/how-get-token.component.ts
+++ src/app/how-get-tocken/how-get-token.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-how-get-token',
   templateUrl: './how-get-token.component.html',
-  styleUrls: ['./how-get-token.component.scss']
+  styleUrls: ['./how-get-token.component.scss'],
 })
 export class HowGetTokenComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
@@ -15,5 +16,12 @@ export class HowGetTokenComponent implements OnInit {
   goToMain() {
     window.location.href = '#/auth/login';
     // window.location.reload();
+    setTimeout(() => {
+      document.getElementById('head6').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      document.getElementById('show-registration').click();
+    }, 300);
   }
 }
